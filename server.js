63376c323a809c7ae4565eb7d7158011dc81ec02
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-10 21:47:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile('article one files will be served here');
+    res.send('article one files will be served here');
 });
 
 app.get('/article-two',function(req,res){
-    res.sendFile('articles two files served here');
+    res.send('articles two files served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
