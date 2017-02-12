f91f6fd706e4dadd3bb5fe6564a9d5c8905eecdc
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-12 08:11:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+//alert box
+alert("hi! i'm gokulnath");
+
+//adding onclick image property
+var img=document.getElementById("dino");
+var marginleft=0;
+
+function moveright(){
+    marginleft=marginleft+10;
+    img.style.marginleft=marginleft+'px';
+}
+img.onclick=function(){
+var interval=setInterval(moveright,100);
+};
