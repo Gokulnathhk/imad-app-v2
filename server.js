ad951b3fd3419b4a5984da5c05dbcae02eb96e2a
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-13 17:37:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,16 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    //get the name from the request
+    var name=req.params.name;
+    names.push(name);
+    
+    //JSON:java script object notation
+    res.send(JSON.stringify(names));//TODO
+});
+
 app.get('/ui/style.css',function(req,res){
     res.sendFile(path.join(__dirname,'ui','style.css'));
 });
