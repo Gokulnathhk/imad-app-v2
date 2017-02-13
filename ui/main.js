0e336812bea3c72104a62d56ca3cc105517e1bbd
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-13 14:46:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,15 @@
 //counter code
-
+var counter=0;
 var button=document.getElementById("counter");
 
 button.onClick=function(){
     //create the request
-     var request=new XMLHttpRequest();
-     
-     //capture the response
-     request.onreadystatechange=function(){
-         if(request.readystate==XMLHttprequest.DONE){
-            //take some actions
-            if(request.status==200){
-            counter=request.responseText;
-            var span=document.getElementById("counter");
-            span.innerHTML=count.toString();
-            }
-     
-            }
-            //not done yet
-         };
    
-    //make the request
-    request.open(GET,"http://gokulnathhk.imad.hasura-app.io",true);
-    request.send(null);
+   //capture the response 
+   
+   //render the request
+   counter=counter+1;
+   var span=document.getElementById("count");
+   span.innerHTML=counter.toString();
+   
 };
