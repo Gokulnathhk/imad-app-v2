c205822f0ab660b6bb01b809c99cfa0bd86629ac
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-13 11:36:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,5 @@
-console.log('Loaded!');
-//adding onclick image property
-var img=document.getElementById("dino");
+//gonna call the counter variable
 
- var marginLeft=0;
- 
-function moveright(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-var interval=setInterval(moveright,50);
-};
+//i'm gonna rock at it
+
+var span=0;
