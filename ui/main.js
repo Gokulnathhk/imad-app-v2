1da892fa0ed22963b63b9b5008ea2e330d200503
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-13 11:45:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,17 @@
+//counter code
+
 //gonna call the counter variable
 
 //i'm gonna rock at it
 
-var span=0;
+var counter=0;
+var button=document.getElementById("counter");
+button.onClick=function(){
+    //request for counter end point.
+    //store it correct span variable 
+    //render the variable
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
+    
+};
