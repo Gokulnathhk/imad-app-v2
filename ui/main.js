795f54b05ff7eaa29a48fe88a2eabf33984ed752
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-13 18:16:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,13 +43,10 @@ submit.onClick=function(){
               } 
          });
 
+        var nameInput=document.getElementById('name');
+        var name=nameInput.value;
+        
+        request.open('GET','http://gokulnathhk.imad.hasura-app.io/submit-name?='+name,true);
+        request.send(null);
 };
-
-};
-
- var nameInput=document.getElementById('name');
-var name=nameInput.value;
-
-request.open('GET','http://gokulnathhk.imad.hasura-app.io/submit-name?='+name,true);
-request.send(null);
 
