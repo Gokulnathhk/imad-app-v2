f19e776a364fe26ae9a02d719d7d342ebe5570a9
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-13 17:25:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,8 +34,8 @@ submit.onClick=function(){
   //capture a list of names and render the names
   var names=['name1','name2','name3'];
   var list='';
-  for(var i=0;i<.names.length;i++){
-      list+=<li>+names[i]+</li>;
+  for(var i=0;i<names.length;i++){
+      list=list+'<li>'+names[i]+'</li>';
   }
   var ul=document.getElementById('nameslist');
   ul.innerHTML=list;
