241a8979554b2a768ee26f0e853d2bc89e1ccc64
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-13 17:43:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,6 +60,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names=[];
+app.get('/submit-name',function(req,res){//URL:/submit-name/query=xxxx
+    //get the name from the request
+    var name=req.query.name;
+    names.push(name);
+    
+    //JSON:java script object notation
+    res.send(JSON.stringify(names));//TODO
+});
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
@@ -76,15 +86,6 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var names=[];
-app.get('/submit-name',function(req,res){//URL:/submit-name/query=xxxx
-    //get the name from the request
-    var name=req.query.name;
-    names.push(name);
-    
-    //JSON:java script object notation
-    res.send(JSON.stringify(names));//TODO
-});
 
 app.get('/ui/style.css',function(req,res){
     res.sendFile(path.join(__dirname,'ui','style.css'));
