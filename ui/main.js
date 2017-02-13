fb0f2bffc44dcfa9f2d801dbb9adb3d70d19029f
Gokulnathhk <gokul104027.cs@rmkcet.ac.in>
2017-02-13 17:20:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,5 +22,21 @@ button.onClick=function(){
    request.open('GET','http://gokulnathhk.imad.hasura-app.io/counter',true);
    request.send(null);
    
-   
+};
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit');
+submit.onClick=function(){
+  //make a request to the server amnd the current entered name
+ 
+  
+  //capture a list of names and render the names
+  var names=['name1','name2','name3'];
+  var list='';
+  for(var i=0;i<.names.length;i++){
+      list+=<li>+names[i]+</li>;
+  }
+  var ul=document.getElementById('nameslist');
+  ul.innerHTML=list;
 };
